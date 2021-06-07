c0d16e5a6115b5d14613c48b6ca6515db80b975a
Zubin Pratap <zubin.pratap@gmail.com>
2021-06-07 10:09:41 +1000

    implement getArnieQuotes(). Unit tests with stubbed network endpoint passing.

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,32 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
-const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+const getArnieQuotes = async urls => {
+    const requestsP = urls.map(url => httpGet(url)); // following naming convention of suffixing vars that refer to promises with a P
+    return Promise.all(requestsP)
+        .then(getResultsFromHttpResponses)
+        .catch(e => {
+            // TODO(zubin): handle network failures (non HTTP response errors) appropriately.  For now, rethrow.
+            throw e;
+        });
+};
+
+const getResultsFromHttpResponses = responses => {
+    const results = [];
+    const successKey = "Arnie Quote";
+    const failureKey = "FAILURE";
+
+    for (const res of responses) {
+        let responseBody = JSON.parse(res.body);
+
+        if (res.status === 200) {
+            results.push({ [successKey]: responseBody.message });
+        } else {
+            results.push({ [failureKey]: responseBody.message });
+        }
+    }
+    return results;
 };
 
 module.exports = {
-  getArnieQuotes,
+    getArnieQuotes,
 };
